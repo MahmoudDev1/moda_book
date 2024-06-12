7818f99ddf7686c3d6926028cea409f61030ce29
ModaPro1 <mahmoudfarghly519@gmail.com>
2024-06-12 13:31:25 +0300

    small changes

--- app/@modal/create-post/page.tsx
+++ app/@modal/create-post/page.tsx
@@ -16,8 +16,8 @@ export default async function Modal() {
       <div className="modal rounded-md bg-white p-3 shadow-md">
         <div className="mb-3 pb-3 border-b flex justify-between items-center">
           <h2 className="text-2xl font-semibold">Create new post</h2>
-          <Link href="/" className="w-8 h-8 rounded-full flex justify-center items-center bg-gray-200">
-            <Image src="assets/x.svg" alt="Close Modal" width={15} height={15} />
+          <Link href="/" className="w-8 h-8 rounded-full flex justify-center items-center bg-gray-200 p-2">
+            <Image src="assets/x.svg" alt="Close Modal" width={10} height={15} className="h-auto w-auto" />
           </Link>
         </div>
         <AddPost />
--- components/Post.tsx
+++ components/Post.tsx
@@ -7,16 +7,20 @@ interface Props {
   image: string | null;
   likes_count: number;
   isLiked: boolean;
-  updateOptimisticPosts: Function
+  updateOptimisticPosts: Function;
 }
 
 export default function Post(props: Props) {
   return (
     <div className="post bg-white p-4 rounded-md shadow-sm mb-4">
       {props.image && (
-        <Image src={props.image} alt={props.text} width={800} height={300} className="w-full h-80 rounded-md" />
+        <div className="img relative h-80">
+          <Image src={props.image} alt={props.text} fill className="rounded-md" priority />
+        </div>
       )}
-      <h4 className={`font-semibold py-2 border-b whitespace-pre-line ${props.image ? "text-md" : "text-xl"}`}>{props.text}</h4>
+      <h4 className={`font-semibold py-2 border-b whitespace-pre-line ${props.image ? "text-md" : "text-xl"}`}>
+        {props.text}
+      </h4>
       <div className="like mt-2 flex items-center gap-2">
         <LikePost id={props.id} isLiked={props.isLiked} updateOptimisticPosts={props.updateOptimisticPosts} />
         <span>{props.likes_count} Likes</span>
