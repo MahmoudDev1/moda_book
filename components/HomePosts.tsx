d3087e852404f6c7c0e46a8216a96962d9e36cdb
ModaPro1 <mahmoudfarghly519@gmail.com>
2024-06-18 19:34:31 +0300

    fixed ts bug

--- components/HomePosts.tsx
+++ components/HomePosts.tsx
@@ -1,8 +1,17 @@
 import Posts from "./Posts";
 import { getPosts } from "@/lib/posts";
 
+interface PostInterface {
+  id: number;
+  text: string;
+  image: string | null;
+  likes_count: number;
+  isLiked: boolean;
+  postSaved: boolean
+}
+
 export default async function HomePosts() {
-  let posts = (await getPosts()).posts;
+  let posts = (await getPosts()).posts as PostInterface[];
   
   return <Posts posts={posts} />;
 }
