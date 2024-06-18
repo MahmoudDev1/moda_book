608bb93b5ba0d3729506eaddc0fa471e805f26ba
ModaPro1 <mahmoudfarghly519@gmail.com>
2024-06-18 19:32:05 +0300

    fixed ts error

--- app/saved-posts/page.tsx
+++ app/saved-posts/page.tsx
@@ -1,8 +1,17 @@
 import Posts from "@/components/Posts";
 import { getSavedPostsData } from "@/lib/posts";
 
+interface PostInterface {
+  id: number;
+  text: string;
+  image: string | null;
+  likes_count: number;
+  isLiked: boolean;
+  postSaved: boolean
+}
+
 export default async function SavedPosts() {
-  let posts = (await getSavedPostsData()).posts;
+  let posts = (await getSavedPostsData()).posts as PostInterface[];
   
   return posts && posts.length > 0 ? <Posts posts={posts} /> : <h2 className="p-3 bg-white shadow-sm rounded-md font-semibold">No saved posts found.</h2> ;
 }
