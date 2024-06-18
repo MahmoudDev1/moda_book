ac34112f2a6201493a213bdd4a02df38765acf4a
ModaPro1 <mahmoudfarghly519@gmail.com>
2024-06-18 19:26:33 +0300

    fix bug temporary

--- app/api/users/[name]/route.tsx
+++ app/api/users/[name]/route.tsx
@@ -1,9 +1,9 @@
-import { searchByName } from "@/lib/user";
-import { NextRequest, NextResponse } from "next/server";
+// import { searchByName } from "@/lib/user";
+// import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextRequest, { params }: { params: { name: string } }, res: NextResponse) {
-  const users = await searchByName(params.name)
-  console.log(users);
+// export async function GET(req: NextRequest, { params }: { params: { name: string } }, res: NextResponse) {
+//   const users = await searchByName(params.name)
+//   console.log(users);
 
-  return Response.json({ test: "test" });
-}
+//   return Response.json({ test: "test" });
+// }
